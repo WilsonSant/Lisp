9a36f01ccb952f0765da3934b9941a84c9cb3930
WilsonSant <wilson@wilson.wilson>
2024-07-24 06:19:59 -0300

    Beautiful-Racket: Working on the Module-Begin

--- racket/beautifulracket/stack/stacker-test.rkt
+++ racket/beautifulracket/stack/stacker-test.rkt
@@ -1,1 +1,4 @@
 #lang reader "stacker.rkt"
+42
+"Olá, mundo"
+#t
--- racket/beautifulracket/stack/stacker.rkt
+++ racket/beautifulracket/stack/stacker.rkt
@@ -3,9 +3,14 @@
 (define (read-syntax path port)
   (define src-lines (port->lines port))
   (define src-datums (format-datums '(handle ~a) src-lines))
-  (define module-datum `(module stacker-mod br
-                          ,@src-datums
-                          ))
+  (define module-datum `(module stacker-mod "stacker.rkt"
+                          ,@src-datums))
   (datum->syntax #f module-datum)
   )
- (provide read-syntax)
+(provide read-syntax)
+
+(define-macro (stacker-module-begin HANDLE-EXPR ...)
+  #'(#%module-begin
+     'HANDLE-EXPR ...
+     ))
+ (provide (rename-out [stacker-module-begin #%module-begin]))
